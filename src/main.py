21d540826c00766e24fa93e109f7ad91c2039a79
Omar Ghani <og@Omars-MBP.cust.communityfibre.co.uk>
2023-06-17 23:27:36 +0100

    main app

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
     df = pd.read_csv('/Users/og/Documents/landbay_tech_task/data/data_task_part_1.csv')
     print(df.head(10))
     print(df.columns.tolist())
-    print(df['PROPERTY_CATEGORY'].unique())
+    print(df['CASE_NUMBER'].nunique())
 
 
     con = sqlite3.connect("landbay.db")
@@ -19,12 +19,10 @@ def main():
     )
 
     cursor = con.cursor()
-    cursor.execute("""SELECT PROPERTY_CATEGORY, COUNT(*) AS category_count
-                        FROM mortgage_data
-                        GROUP BY PROPERTY_CATEGORY
-                        ORDER BY category_count DESC
-                        LIMIT 3;
-                  """)
+    cursor.execute("""
+    SELECT COUNT(DISTINCT BROKER_ID) as "Unique Broker ID"
+    FROM mortgage_data
+    """)
     print(cursor.fetchall())
 
 
