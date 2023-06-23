8bfcc452d63cc0055538ddb8dd4d705d912076fa
Omar Ghani <og@Omars-MacBook-Pro.local>
2023-06-23 23:51:57 +0100

    added changes

--- src/main.py
+++ src/main.py
@@ -5,10 +5,6 @@ import sqlite3
 
 def main():
     df = pd.read_csv('/Users/og/Documents/landbay_tech_task/data/data_task_part_1.csv')
-    print(df.head(10))
-    print(df.columns.tolist())
-    print(df['CASE_NUMBER'].nunique())
-
 
     con = sqlite3.connect("landbay.db")
 
@@ -20,9 +16,25 @@ def main():
 
     cursor = con.cursor()
     cursor.execute("""
-    SELECT COUNT(DISTINCT BROKER_ID) as "Unique Broker ID"
-    FROM mortgage_data
-    """)
+    SELECT SUM(total_commission) AS total_commission
+        FROM (
+    SELECT BROKER_ID,
+        SUM(
+            CASE
+                WHEN case_count <= 15 THEN LOAN_AMOUNT * 0.01
+                WHEN case_count <= 30 THEN LOAN_AMOUNT * 0.03
+            ELSE LOAN_AMOUNT * 0.05
+        END
+        ) AS total_commission
+    FROM (
+        SELECT BROKER_ID, LOAN_AMOUNT,
+            ROW_NUMBER() OVER (PARTITION BY BROKER_ID ORDER BY APPLICATION_SUBMITTED_DATE) AS case_count
+        FROM mortgage_data
+        WHERE strftime('%Y', APPLICATION_SUBMITTED_DATE) = '2021' AND BROKER_ID = '00ca468e-3630-4fe6-b0fd-86f6d27e3b40'
+        ) 
+    GROUP BY BROKER_ID
+);
+""")
     print(cursor.fetchall())
 
 
